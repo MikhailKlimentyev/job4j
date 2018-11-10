2b06bba512aec54dff689d40c6f5cba95f5d5e9c
Mikhail Klimentyev <mikhail.klimentyev@gmail.com>
2018-11-10 16:03:24 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+ * Class for ptinting "Hello World!!!" into console.
+ * @author Mikhail Klimentyev
+ * @since 10/11/2018
+ * @version 1.0
+ */
 public class Calculate {
+	/**
+	 * main.
+	 * @param args - String[] args.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for calculate task.
+ * @author Mikhail Klimentyev
+ * @since 10/11/2018
+ * @version 1.0
+ */
+package ru.job4j.calculate;
