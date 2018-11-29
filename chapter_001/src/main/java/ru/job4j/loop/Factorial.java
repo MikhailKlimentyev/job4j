3546277951e65f82f62dfe2847bca36795544d68
Mikhail Klimentsyeu <Mikhail_Klimentsyeu@epam.com>
2018-11-29 18:41:39 +0300

    Delete checking for 0

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -13,17 +13,12 @@ public class Factorial {
      * calc.
      * <p>
      * Method gets number and returns factorial of this number.
-     * <p>
-     * If method gets zero, it returns 1.
      *
      * @param n number.
      * @return factorial.
      */
     public int calc(int n) {
         int result = 1;
-        if (n == 0) {
-            result = 1;
-        }
         for (int i = 1; i <= n; i++) {
             result *= i;
         }
