f24f89b16fed590fdb9a9f391a3b80923fa0d4f4
Mikhail Klimentsyeu <Mikhail_Klimentsyeu@epam.com>
2018-11-30 23:53:31 +0300

    5.4. Построить пирамиду в псевдографике.

---
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -0,0 +1,92 @@
+package ru.job4j.loop;
+
+/**
+ * Paint.
+ *
+ * @author Mikhail Klimentyev
+ * @version $Id$
+ * @since 11/30/2018
+ */
+public class Paint {
+
+    /**
+     * rightTr.
+     * <p>
+     * Method creates right triangle.
+     *
+     * @param height height.
+     * @return right triangle.
+     */
+    public String rightTrl(int height) {
+        // Буфер для результата.
+        StringBuilder screen = new StringBuilder();
+        // ширина будет равна высоте.
+        int width = height;
+        // внешний цикл двигается по строкам.
+        for (int row = 0; row != height; row++) {
+            // внутренний цикл определяет положение ячейки в строке.
+            for (int column = 0; column != width; column++) {
+                // если строка равна ячейке, то рисуем галку.
+                // в данном случае строка определяет, сколько галок будет на строке
+                if (row >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            // добавляем перевод строки.
+            screen.append(System.lineSeparator());
+        }
+        // Получаем результат.
+        return screen.toString();
+    }
+
+    /**
+     * leftTrl.
+     * <p>
+     * Method creates left triangle.
+     *
+     * @param height height.
+     * @return left triangle.
+     */
+    public String leftTrl(int height) {
+        StringBuilder screen = new StringBuilder();
+        int width = height;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != width; column++) {
+                if (row >= width - column - 1) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+
+    /**
+     * pyramid.
+     * <p>
+     * Method creates isosceles triangle.
+     *
+     * @param height height.
+     * @return isosceles triangle.
+     */
+    public String pyramid(int height) {
+        StringBuilder screen = new StringBuilder();
+        int width = 2 * height - 1;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != width; column++) {
+                if (row >= height - column - 1 && row + height - 1 >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+
+}
