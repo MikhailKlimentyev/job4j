fa3097db770e44f55d3807932adbe1c789f4dcdc
Mikhail Klimentsyeu <Mikhail_Klimentsyeu@epam.com>
2018-12-03 09:41:47 +0300

    Fix condition in for loop

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -19,7 +19,7 @@ public class Check {
      */
     public boolean mono(boolean[] data) {
         boolean result = true;
-        for (int index = 0; index != data.length - 1; index++) {
+        for (int index = 0; index != data.length; index++) {
             if (data[0] != data[index]) {
                 result = false;
                 break;
