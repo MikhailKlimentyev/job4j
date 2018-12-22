937c5f15cb0e1770ebdd23f9f012ce093003474c
Mikhail Klimentyev <mikhail.klimentyev@gmail.com>
2018-12-22 20:04:41 +0300

    Fix tests Экзамен. Базовый синтаксис.

--- chapter_001/src/main/java/ru/job4j/exam/ThirdSortedArray.java
+++ chapter_001/src/main/java/ru/job4j/exam/ThirdSortedArray.java
@@ -45,7 +45,6 @@ public class ThirdSortedArray {
             secondIndex++;
             thirdIndex++;
         }
-
         return third;
     }
 }
--- chapter_001/src/test/java/ru/job4j/exam/ThirdSortedArrayTest.java
+++ chapter_001/src/test/java/ru/job4j/exam/ThirdSortedArrayTest.java
@@ -14,11 +14,21 @@ import static org.junit.Assert.assertThat;
  */
 public class ThirdSortedArrayTest {
     @Test
-    public void whenTwoSortedAscendArraysThenThirdAscendArray() {
+    public void whenTwoSortedAscendArraysSameLengthThenThirdAscendArray() {
         ThirdSortedArray thirdSortedArray = new ThirdSortedArray();
         int[] first = {3, 7, 8, 9, 10};
         int[] second = {1, 3, 5, 7, 13};
         int[] expected = {1, 3, 3, 5, 7, 7, 8, 9, 10, 13};
+        int[] result = thirdSortedArray.getArray(first, second);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenTwoSortedAscendArraysDifferentLengthThenThirdAscendArray() {
+        ThirdSortedArray thirdSortedArray = new ThirdSortedArray();
+        int[] first = {-3, 0, 1, 9, 10, 15};
+        int[] second = {0, 1, 9, 9};
+        int[] expected = {-3, 0, 0, 1, 1, 9, 9, 9, 10, 15};
         int[] result = thirdSortedArray.getArray(first, second);
         assertThat(result, is(expected));
     }
