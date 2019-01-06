e9413e5f828ce96195600aa9b1bd513d3ebc0533
Mikhail Klimentyev <mikhail.klimentyev@gmail.com>
2019-01-06 20:07:40 +0300

    3. if-else-throw [#49895]

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -51,10 +51,9 @@ public class ConsoleInput implements Input {
                 break;
             }
         }
-        if (exist) {
-            return key;
-        } else {
+        if (!exist) {
             throw new MenuOutException("Out of menu range.");
         }
+        return key;
     }
 }
