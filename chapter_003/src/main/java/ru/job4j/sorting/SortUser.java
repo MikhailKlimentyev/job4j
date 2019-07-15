c6d8370554854489e9898ca477399952a7dd5d91
Mikhail Klimentyev <mikhail.klimentyev@gmail.com>
2019-07-15 22:32:37 +0300

    2. Сортировка User с использованием Comparator

--- chapter_003/src/main/java/ru/job4j/sorting/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sorting/SortUser.java
@@ -1,5 +1,7 @@
 package ru.job4j.sorting;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -8,5 +10,30 @@ public class SortUser {
 
     public Set<User> sort(List<User> users) {
         return new TreeSet<>(users);
+    }
+
+    public List<User> sortNameLength(List<User> users) {
+        Collections.sort(users, new Comparator<User>() {
+            @Override
+            public int compare(User firstUser, User secondUser) {
+                return firstUser.getName().length() - secondUser.getName().length();
+            }
+        });
+        return users;
+    }
+
+    public List<User> sortByAllFields(List<User> users) {
+        Collections.sort(users, new Comparator<User>() {
+            @Override
+            public int compare(User firstUser, User secondUser) {
+                int comparison;
+                comparison = firstUser.getName().compareTo(secondUser.getName());
+                if (comparison == 0) {
+                    comparison = firstUser.getAge().compareTo(secondUser.getAge());
+                }
+                return comparison;
+            }
+        });
+        return users;
     }
 }
--- chapter_003/src/test/java/ru/job4j/sorting/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sorting/SortUserTest.java
@@ -6,6 +6,7 @@ import org.hamcrest.collection.IsIterableContainingInOrder;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 public class SortUserTest {
@@ -20,5 +21,30 @@ public class SortUserTest {
 
         Set<User> actual = sortUser.sort(Arrays.asList(alex, ivan, roman));
         assertThat(actual, IsIterableContainingInOrder.contains(roman, alex, ivan));
+    }
+
+    @Test
+    public void testWhenUserHasShorterNameThenUserPlacedEarlier() {
+        SortUser sortUser = new SortUser();
+
+        User alex = new User("Alex", 33);
+        User ivan = new User("Ivan", 65);
+        User roman = new User("Roman", 32);
+
+        List<User> actual = sortUser.sortNameLength((Arrays.asList(alex, ivan, roman)));
+        assertThat(actual, IsIterableContainingInOrder.contains(alex, ivan, roman));
+    }
+
+    @Test
+    public void testWhenUserHasNameAndAgeThenSortByNameLexicographicallyAndThenSortByAge() {
+        SortUser sortUser = new SortUser();
+
+        User sergey25 = new User("Sergey", 25);
+        User ivan30 = new User("Ivan", 30);
+        User sergey20 = new User("Sergey", 20);
+        User ivan25 = new User("Ivan", 25);
+
+        List<User> actual = sortUser.sortByAllFields((Arrays.asList(sergey25, ivan30, sergey20, ivan25)));
+        assertThat(actual, IsIterableContainingInOrder.contains(ivan25, ivan30, sergey20, sergey25));
     }
 }
