fe9e61f1025e9fa36465a644701db1a074ec9643
Mikhail Klimentyev <mikhail.klimentyev@gmail.com>
2019-07-28 18:11:09 +0300

    Fix checkstyle

--- chapter_003/src/main/java/ru/job4j/sorting/ListCompare.java
+++ chapter_003/src/main/java/ru/job4j/sorting/ListCompare.java
@@ -7,24 +7,24 @@ public class ListCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         int minLength;
-        int lengthСomparison;
+        int lengthComparison;
         if (left.length() == right.length()) {
             minLength = left.length();
-            lengthСomparison = 0;
+            lengthComparison = 0;
         } else if (left.length() < right.length()) {
             minLength = left.length();
-            lengthСomparison = -1;
+            lengthComparison = -1;
         } else {
             minLength = right.length();
-            lengthСomparison = 1;
+            lengthComparison = 1;
         }
         for (int index = 0; index != minLength; index++) {
-            int symbolСomparison = Character.compare(left.charAt(index), right.charAt(index));
-            if (symbolСomparison != 0) {
-                return symbolСomparison;
+            int symbolComparison = Character.compare(left.charAt(index), right.charAt(index));
+            if (symbolComparison != 0) {
+                return symbolComparison;
             }
         }
-        return lengthСomparison;
+        return lengthComparison;
     }
 }
 
