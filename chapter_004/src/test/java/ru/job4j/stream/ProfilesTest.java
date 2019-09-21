ecc1421ed4f700d0583b2f947e10dd50441685db
Mikhail Klimentyev <mikhail.klimentyev@gmail.com>
2019-09-21 16:31:59 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- chapter_004/src/test/java/ru/job4j/stream/ProfilesTest.java
+++ chapter_004/src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -33,4 +33,29 @@ public class ProfilesTest {
         List<Address> expectedAddresses = Arrays.asList(vitebsk, minsk, liverpool, milan);
         assertThat(addresses, is(expectedAddresses));
     }
+
+    /**
+     * T.
+     */
+    @Test
+    public void whenProfilesThenUniqueAddresses() {
+        Address firstVitebsk = new Address("Vitebsk", "3434", 4, 4);
+        Address secondVitebsk = new Address("Vitebsk", "3434", 4, 4);
+        Address minsk = new Address("Minsk", "ddd", 5, 6);
+        Address firstLiverpool = new Address("Liverpool", "fhrfhr", 55, 566);
+        Address secondLiverpool = new Address("Liverpool", "fhrfhr", 55, 566);
+        Address milan = new Address("Milan", "eheh", 44, 44);
+        List<Profile> profileList = Arrays.asList(
+            new Profile(firstVitebsk),
+            new Profile(secondVitebsk),
+            new Profile(minsk),
+            new Profile(firstLiverpool),
+            new Profile(secondLiverpool),
+            new Profile(milan)
+        );
+        Profiles profiles = new Profiles();
+        List<Address> addresses = profiles.collectUniqueAddresses(profileList);
+        List<Address> expectedAddresses = Arrays.asList(firstLiverpool, milan, minsk, firstVitebsk);
+        assertThat(addresses, is(expectedAddresses));
+    }
 }
